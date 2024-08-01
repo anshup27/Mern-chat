5ac044f46e0fe7aa9e257d5855b5dcf7ed35dc82
anshup27 <154739084+anshup27@users.noreply.github.com>
2024-08-01 12:01:08 +0530

    Tough

--- front/src/App.js
+++ front/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Chatpage from "./Pages/Chatpage";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Route path="/" Component={Homepage} />
-      <Route path="/chats" Component={Chatpage} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/chats" element={<Chatpage />} />
+    </Routes>
   );
 }
 
